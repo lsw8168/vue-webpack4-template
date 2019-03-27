df958e734f30d2e5cf78e05231ef3b47f55e82c9
lsw8168 <lsw8168@naver.com>
2019-03-27 10:31:04 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container center-content">
+    <router-link to="/">Home</router-link>
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+.container {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    max-width: 1200px;
+}
+</style>
