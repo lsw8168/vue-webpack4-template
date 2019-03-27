860a93a17b08d32e1198060e942e35e9a8aabdfd
lsw8168 <lsw8168@naver.com>
2019-03-27 14:44:57 +0900

    Add axios

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,55 @@
   <div class="container center-content">
     <router-link to="/">Home</router-link>
     <router-view />
+    <div id="Btn">
+      <div v-if="hasResult">
+        <div
+          v-for="post in posts"
+          :key="post.id"
+        >
+          <h1>{{ post.title }}</h1>
+          <p>{{ post.body }}</p>
+        </div>
+      </div>
+      <button
+        v-else
+        @click="searchBtn"
+      >글 불러오기</button>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'Btn',
+  data: function () {
+    return {
+      posts: []
+    }
+  },
+  computed: {
+    hasResult: function () {
+      return this.posts.length > 0
+    }
+  },
+  methods: {
+    searchBtn: function () {
+      const baseURI = 'https://jsonplaceholder.typicode.com'
+      this.$http.get(`${baseURI}/posts`).then((result) => {
+        console.log(result)
+        this.posts = result.data
+      }).catch((ex) => {
+        console.log('ERR', ex)
+      })
+    }
+  }
+}
+
+</script>
+
 <style scoped>
 .container {
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    max-width: 1200px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  max-width: 1200px;
 }
 </style>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import router from './router'
 import App from './App'
+import axios from 'axios'
 
 import 'assets/css/app.css'
+
+Vue.prototype.$http = axios
 
 /* eslint-disable-next-line no-new */
 new Vue({
